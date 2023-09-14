a39ae4d76cae588097d96b283d519c0e9e1e6e96
Odimayo David <odimayodavid7@gmail.com>
2023-09-14 18:45:16 +0100

    Removed auth route so all routes can be in one file

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import AuthController from '../controllers/AuthController';
-
-const authroute = express.Router();
-
-authroute.post('/login', AuthController.login)
-
-export default authroute;
