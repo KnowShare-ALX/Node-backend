9c7823f92845353abab9089c8be27fdfceddb5dc
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-09-14 12:45:24 +0100

    db adjustment

--- utils/db.js
+++ utils/db.js
@@ -6,11 +6,14 @@ export class DBClient {
         const host = process.env.Host || 'localhost';
         const port = process.env.PORT || 27017;
         const dataBase = process.env.DATA_BASE || 'know_share';
-        mongoose.connect(`mongodb://${host}:${port}/${dataBase}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            family: 4
-        });
+        const uri = process.env.DB_URI || `mongodb://${host}:${port}/${dataBase}`;
+        mongoose.connect(uri,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                family: 4,
+              }
+            );
         const db = mongoose.connection;
         db.on('error', console.error.bind(console, 'MongoDB connection error:'));
         db.once('open', () => {
