12750bc55f028450f6a4e45670c294fc34db5add
Odimayo David <odimayodavid7@gmail.com>
2023-09-15 13:29:07 +0100

    Installed express-session and cookie parser

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import AuthController from '../controllers/AuthController';
+import isAuthenticated from '../middlewares/AuthMiddleware';
 
 const router = express.Router();
 
 router.post('/login', AuthController.login)
 router.post('/signup', AuthController.signup)
+
+// This route is for testing the authentication middleware
+// it will be removed
+router.get('/', isAuthenticated, (req, res) => {
+    const user = req.user
+    res.send(`home page accessed as ${user.email}`)
+})
 
 export default router;
