5f3e09ca959c08cbefb3041e571b5d4c4ba5be79
Odimayo David <odimayodavid7@gmail.com>
2023-09-15 21:52:19 +0100

    Added logout route

--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,17 @@
-import express from 'express'
+import express, { application } from 'express'
 import AuthController from '../controllers/AuthController';
-import isAuthenticated from '../middlewares/AuthMiddleware';
+import authenticateJWT from '../middlewares/AuthMiddleware';
 
 const router = express.Router();
 
 router.post('/login', AuthController.login)
 router.post('/signup', AuthController.signup)
+router.post('/logout', authenticateJWT, AuthController.logout)
 
 // This route is for testing the authentication middleware
 // it will be removed
-router.get('/', isAuthenticated, (req, res) => {
-    const user = req.user
-    res.send(`home page accessed as ${user.email}`)
+router.get('/', authenticateJWT, (req, res) => {
+    res.send(`home page accessed`)
 })
 
 export default router;
