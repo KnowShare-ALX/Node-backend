5a5093b146aa5bb153c9c5741cf34ec5b66d219a
Odimayo David <odimayodavid7@gmail.com>
2023-09-15 21:52:56 +0100

    Updated the middleware to check if a token is still valid via the redis cache

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,10 +1,35 @@
-function isAuthenticated(req, res, next) {
-    // Check if the user is authenticated (e.g., through Passport.js)
-    if (req.isAuthenticated()) {
-      return next(); // User is authenticated, proceed to the next middleware
-    } else {
-      return res.status(401).json({ error: 'Unauthorized' }); // User is not authenticated, send a 401 Unauthorized response
-    }
-}
+import passport from 'passport';
+import redisClient from '../utils/redis';
 
-export default isAuthenticated;
+const authenticateJWT = (req, res, next) => {
+    passport.authenticate('jwt', { session: false }, (err, user) => {
+      if (err || !user) {
+        return res.status(401).json({ error: 'Authentication failed' });
+      }
+      
+      console.log(user)
+      const token = req.headers.authorization.split(' ')[1]
+      if (redisClient.isAlive()) {
+        const user_id = redisClient.get(`auth_${token}`);
+        user_id.then((value) => {
+            console.log('value is', value)
+            if (!value) {
+                console.log('In here')
+                return res.status(401).json({ error: 'Authentication failed token invalid' })
+            }
+            // If authentication is successful, set the user on the request object
+            req.user = user;
+        
+            // Continue to the next middleware or route handler
+            next();
+        }).catch((error) => {
+            console.log(error)
+            return res.status(500).json({ error: 'something went wrong' });
+        })
+      }else {
+        return res.status(500).json({ error: 'Redis is not available' });
+      }
+    })(req, res);
+  };
+
+export default authenticateJWT;
