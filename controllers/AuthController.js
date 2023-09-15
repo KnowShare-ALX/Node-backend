ed78740c80a802a13fbb23587f223f1b6c8cf2d5
Odimayo David <odimayodavid7@gmail.com>
2023-09-15 13:32:37 +0100

    Created the passport user serailizer and deserializer - Implemented the login with passport

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,23 +1,52 @@
 import User from '../models/user';
 import bycrypt from 'bcrypt';
+import passport from 'passport';
+
+const saltRounds = 10
 
 class AuthController {
     static async login(req, res) {
-        
+        const { email, password } = req.body
+        User.findOne({ email })
+            .then(async (user) => {
+                if (!user) {
+                    res.status(404).json({ error: 'User does not exist' });
+                    return Promise.reject('User not found'); 
+                }
+                const isCorrect = await bycrypt.compare(password, user.password);
+                if (!isCorrect) {
+                    res.status(401).json({ error: 'Incorrect Password' });
+                    return;
+                }
+                req.login(user._id, (err) => {
+                    if (err) {
+                        console.error('Error logging in:', err);
+                        res.status(500).json({ error: 'Internal server error' });
+                        return;
+                    }
+                    res.status(200).json({ message: 'Logged In successfully' })
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+                if (error !== 'User not found') {
+                    res.status(500).json({ error: 'Internal server error' });
+                }
+            })
     }
 
     static async signup(req, res) {
         try {
             const { email, password, firstname, lastname, state, country } = req.body;
             User.findOne({ email })
-                .then((user) => {
+                .then(async (user) => {
                     console.log(user)
                     if (user) {
                         res.status(409).json({ message: 'User already exists' })
                         return Promise.reject('User already exists')
                     }
 
-                    return bycrypt.hash(password, 10);
+                    return await bycrypt.hash(password, saltRounds);
                 })
                 .then((hashedPassword) => {
                     const userObj = {
@@ -53,5 +82,19 @@ class AuthController {
         }
     }
 }
+
+passport.serializeUser((user_id, done) => {
+    done(null, user_id);
+})
+
+passport.deserializeUser((user_id, done) => {
+    User.findById(user_id)
+        .then((user) => {
+            done(null, user);
+        })
+        .catch((error) => {
+            done(error, null);
+        })
+})
 
 export default AuthController;
