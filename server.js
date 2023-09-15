d2aeb4a174d198adf451f5fb467a3528d2045765
Odimayo David <odimayodavid7@gmail.com>
2023-09-15 21:50:22 +0100

    Removed unecessary session based auth imports

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ require('dotenv').config();  // To access .env file contents
 import express from 'express';
 import router from './routes/main';
 import dbClient from './utils/db';
-import session from 'express-session';
 import passport from 'passport';
-import cookieParser from 'cookie-parser'
 
  const app = express();
 
@@ -12,15 +10,7 @@ const PORT = process.env.PORT || 3000; // port 3000 for dev purposes
 
 dbClient.db.on('connected', () => {
     app.use(express.json());
-    app.use(cookieParser())
-    app.use(session({
-        secret: process.env.SECRET_KEY,
-        resave: false,
-        saveUninitialized: false,
-        // cookie: {secure: true},
-    }))
     app.use(passport.initialize())
-    app.use(passport.session())
     app.use('/', router);
 
     app.listen(PORT, () => {
