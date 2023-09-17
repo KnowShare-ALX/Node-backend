bf40c8409661316c80e6eba45f01574e45014313
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-09-17 17:21:22 +0100

    fix

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require('dotenv').config();  // To access .env file contents
 const express = require('express');
 const router = require('./routes/main');
 import dbClient from './utils/db';
+import redisClient from './utils/redis';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000; // port 3000 for dev purposes
 
 dbClient.db.on('connected', () => {
+    redisClient.isAlive();
     app.use(express.json());
     app.use('/', router);
     app.listen(PORT, () => {
