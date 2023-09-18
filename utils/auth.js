97877bc7a7182d9fece632c421be04d1d9ad7f51
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-09-18 07:43:42 +0100

    fix

--- utils/auth.js
+++ utils/auth.js
@@ -3,14 +3,6 @@ const passport = require('passport');
 const passportJWT = require('passport-jwt');
 const jwt = require('jsonwebtoken');
 
-const JwtStrategy = passport.Strategy;
-const ExtractJwt = passportJWT.ExtractJwt;
-
-const JwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrket: process.env.TOKEN_SECRET,
-};
-
 export default class AuthHandler {
     static generateAcessToken(userDetails) { //userDetails should be an object {email}
         return jwt.sign(
