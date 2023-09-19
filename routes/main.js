c2fd8fb37d0227dc07e0415a3e3633e85d260def
Odimayo David <odimayodavid7@gmail.com>
2023-09-19 09:02:10 +0100

    Removed the testing route

--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,11 @@
+import express from 'express'
 import AuthController from '../controllers/AuthController';
 import authenticateJWT from '../middlewares/AuthMiddleware';
-const express = require('express');
 
 const router = express.Router();
 
 router.post('/login', AuthController.login)
 router.post('/signup', AuthController.signup)
-router.get('/logout', authenticateJWT, AuthController.logout)
-
-// This route is for testing the authentication middleware
-// it will be removed
-router.get('/', authenticateJWT, (req, res) => {
-    res.status(200).json({
-        message: 'Home page accessed successfully!',
-        user: req.user
-    });
-})
+router.post('/logout', authenticateJWT, AuthController.logout)
 
 export default router;
