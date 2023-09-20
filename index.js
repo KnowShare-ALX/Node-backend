0bdb5b1c8c94a7e3b2e605a2e1852bbba1bd2186
Odimayo David <odimayodavid7@gmail.com>
2023-09-20 13:18:49 +0100

    Updated swagger css to fix swagger deployment

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const swaggerOptions = {
 };
   
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
   
 
 const PORT = process.env.PORT || 3000; // port 3000 for dev purposes
@@ -28,7 +29,7 @@ dbClient.client.on('connected', () => {
     app.use(express.json());
     app.use(passport.initialize())
     app.use('/', router);
-    app.use('/api-docs', serve, setup(swaggerDocs));
+    app.use('/api-docs', serve, setup(swaggerDocs, { customCssUrl: CSS_URL }));
     app.listen(PORT, () => {
         console.log(`Server is running on Port ${PORT}`);
     })
