b325d57d5e5f80f1a2549e7a53738cb80d88c5fb
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-09-21 13:22:48 +0100

    hot fix

---
+++ middlewares/CorsMiddleware.js
@@ -0,0 +1,15 @@
+function corsMiddleware(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Expose-Headers", "x-auth-token");
+    if (req.method == "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+}
+
+export default corsMiddleware;
