1aa1b9e9301c5186216a944deb7cf616e3023b0c
Odimayo David <odimayodavid7@gmail.com>
2023-09-22 14:25:55 +0100

    Swagger fix

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ const swaggerOptions = {
         },
       ],
     },
-    apis: ["./docs/**/*.yaml"],
+    apis: ["./docs/*.yaml"],
 };
   
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
   
 
 const PORT = process.env.PORT || 3000; // port 3000 for dev purposes
@@ -33,10 +34,10 @@ const PORT = process.env.PORT || 3000; // port 3000 for dev purposes
 
 dbClient.client.on('connected', () => {
   app.use(express.json());
-  app.use(passport.initialize())
-  app.use(corsMiddleware)
-  app.use('/api-docs', serve, setup(swaggerDocs));
+  app.use(passport.initialize());
+  app.use(corsMiddleware);
   app.use('/', router);
+  app.use('/api-docs', serve, setup(swaggerDocs, { customCssUrl: CSS_URL }));
   app.listen(PORT, () => {
       console.log(`Server is running on Port ${PORT}`);
   })
